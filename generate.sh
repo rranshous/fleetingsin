9d936b524a17bc076f72f9550f8cff504a2afb31
Robby Ranshous <rranshous@gmail.com>
2014-09-21 21:39:23 -0400

    reworked generater

--- generate.sh
+++ generate.sh
@@ -5,18 +5,23 @@ short_domain=$2
 image=$3
 
 # create output dir
-mkdir -p ${full_domain}
+out_dir=../${full_domain}
+mkdir -p $out_dir
+echo "OUTPUT TO: ${out_dir}"
 
-# copy service files
-cp domain.com@.service ${full_domain}/${full_domain}@.service
-cp domain.com-register@.service ${full_domain}/${full_domain}-register@.service
+# copy all files
+cp -r . $out_dir
+
+# rename service files
+mv $out_dir/domain.com@.service $out_dir/${full_domain}@.service
+mv $out_dir/domain.com-register@.service $out_dir/${full_domain}-register@.service
 
 # replace all occurances of tokens
-sed -i -e s/DOMAIN.COM/"${full_domain}"/g ${full_domain}/${full_domain}@.service
-sed -i -e s/DOMAIN.COM/"${full_domain}"/g ${full_domain}/${full_domain}-register@.service
+sed -i -e s/DOMAIN.COM/"${full_domain}"/g $out_dir/${full_domain}@.service
+sed -i -e s/DOMAIN.COM/"${full_domain}"/g $out_dir/${full_domain}-register@.service
 
-sed -i -e s/DOMAIN/"${short_domain}"/g ${full_domain}/${full_domain}@.service
-sed -i -e s/DOMAIN/"${short_domain}"/g ${full_domain}/${full_domain}-register@.service
+sed -i -e s/DOMAIN/"${short_domain}"/g $out_dir/${full_domain}@.service
+sed -i -e s/DOMAIN/"${short_domain}"/g $out_dir/${full_domain}-register@.service
 
-sed -i -e s/DOCKER_IMAGE/$(echo $image | sed -e 's/[\/&]/\\&/g')/g ${full_domain}/${full_domain}@.service
-sed -i -e s/DOCKER_IMAGE/$(echo $image | sed -e 's/[\/&]/\\&/g')/g ${full_domain}/${full_domain}-register@.service
+sed -i -e s/DOCKER_IMAGE/$(echo $image | sed -e 's/[\/&]/\\&/g')/g $out_dir/${full_domain}@.service
+sed -i -e s/DOCKER_IMAGE/$(echo $image | sed -e 's/[\/&]/\\&/g')/g $out_dir/${full_domain}-register@.service
